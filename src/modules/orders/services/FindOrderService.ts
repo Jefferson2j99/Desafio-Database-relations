f0305822c68842dbcdc50eef92e4554b8df9a5c4
Nelio Alves <jefferson2112@hotmail.com>
2020-07-28 20:24:54 -0300

    Resolução

--- src/modules/orders/services/FindOrderService.ts
+++ src/modules/orders/services/FindOrderService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import AppError from '@shared/errors/AppError';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
 
@@ -14,12 +15,22 @@ class FindOrderService {
   constructor(
     @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ id }: IRequest): Promise<Order | undefined> {
     const order = await this.ordersRepository.findById(id);
 
-    return order
+    if (!order) {
+      throw new AppError('Order not found');
+    }
+
+    return order;
   }
 }
 
